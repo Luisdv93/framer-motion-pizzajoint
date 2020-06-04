1dc80f40efba0a418300e3de3deba6c64361405c
Luis Vilchez <luisdv93@gmail.com>
2020-06-04 16:05:31 -0400

    Updates

---
+++ precache-manifest.f8d5e242f11e1fbf2bf10eee25f86305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6328c978812f9eace28c05eaa85a6df7",
+    "url": "/framer-motion-pizzajoint/index.html"
+  },
+  {
+    "revision": "fe389cb483b9281cba7c",
+    "url": "/framer-motion-pizzajoint/static/css/main.f8a366a4.chunk.css"
+  },
+  {
+    "revision": "f76820082a72b895b037",
+    "url": "/framer-motion-pizzajoint/static/js/2.6f95a39c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/framer-motion-pizzajoint/static/js/2.6f95a39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe389cb483b9281cba7c",
+    "url": "/framer-motion-pizzajoint/static/js/main.053d8123.chunk.js"
+  },
+  {
+    "revision": "e19e5eaf318eea900163",
+    "url": "/framer-motion-pizzajoint/static/js/runtime-main.c697d697.js"
+  }
+]);
